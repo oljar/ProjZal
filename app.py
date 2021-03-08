79b1a2fa995410fbdabe3f2ece440325ad354f93
oljar <oljar@tlen.pl>
2021-03-08 22:53:40 +0100

    first

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+import data_operation
+
+app = Flask(__name__)
+
+api = Api(app)
+
+data_operation.create_db()
+
+items = []
+
+class Item (Resource):
+
+    def get(self,name):
+        solution= []
+        for item in items :
+            if item['name']== name:
+                solution.append(item)
+        return solution
+
+
+
+
+    def post (self,name):
+        data = request.get_json()
+        item = {'name' : name , 't1' : data['t1'], 't2' : data['t2'], 't3' :data['t3'] ,'t4': data['t4']}
+        items.append(item)
+        data_operation.add_data_db(name,data['t1'],data['t2'],data['t3'],data['t4'])
+
+        return item,201
+
+class ItemList(Resource):
+
+    def get (self):
+        return data_operation.get_data_db()
+
+class ItemListName(Resource):
+
+    def get (self,name):
+        return data_operation.get_data_name_db(name)
+
+
+
+
+@app.route('/')
+def aaa():
+        return 'ok'
+
+api.add_resource(Item,'/<string:name>')
+api.add_resource(ItemList,'/list')
+api.add_resource(ItemListName,'/list/<string:name>')
+
+app.run()
+
+
---
+++ data_operation.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+
+def create_db():
+
+    conn =sqlite3.connect("data.db")
+    c=conn.cursor()
+    query = """
+        DROP TABLE IF EXISTS "stock";
+        CREATE TABLE "stock" (
+        "id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+        "name"  TEXT,
+        "t1"	REAL,
+        "t2"	REAL,
+        "t3"    REAL,
+        "t4"	REAl
+    );
+    """
+    c.executescript(query)
+    conn.commit()
+    conn.close()
+
+def add_data_db(name,t1,t2,t3,t4):
+    conn =sqlite3.connect("data.db")
+    c=conn.cursor()
+    query = """
+         INSERT INTO "stock" ("name", "t1", "t2", "t3", "t4")
+         VALUES (:name, :t1, :t2, :t3, :t4);       
+"""
+
+    item = {"name":name, "t1":t1, "t2":t2, "t3":t3,"t4":t4 }
+
+    c.execute(query, item)
+
+    conn.commit()
+    conn.close()
+
+
+def get_data_db():
+    conn =sqlite3.connect("data.db")
+    c=conn.cursor()
+    query = """
+    SELECT * FROM "stock";
+    """
+    c.execute(query)
+    item =c.fetchall()
+    return item
+
+def get_data_name_db(name):
+    conn =sqlite3.connect("data.db")
+    c=conn.cursor()
+    query = """
+    SELECT * FROM "stock" WHERE "name" = ?;
+    """
+    name=name
+    c.execute(query,(name,))
+    item =c.fetchall()
+    return item
