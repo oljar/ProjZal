358cfb56e1354b514f46b25008633214f62616a2
oljar <oljar@tlen.pl>
2021-03-29 21:20:31 +0200

    index02

--- app.py
+++ app.py
@@ -97,10 +97,10 @@ api.add_resource(ItemListName, '/list/<string:name>')
 
 # app.run(debug=True)
 
-#
-if __name__ == "__main__":
-#port = int(os.environ.get("PORT", 5000))
-#    #app.run(host='0.0.0.0', port=port)
-#
-    app.run(debug=True)
-#app.run()
+# #
+# if __name__ == "__main__":
+# #port = int(os.environ.get("PORT", 5000))
+# #    #app.run(host='0.0.0.0', port=port)
+# #
+#     app.run(debug=True)
+# #app.run()
